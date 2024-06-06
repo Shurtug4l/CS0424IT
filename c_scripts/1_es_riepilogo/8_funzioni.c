2b413a75f3533b45c017650fa92462a891879fa2
Simone <92377343+simone0509@users.noreply.github.com>
2024-06-06 11:48:54 +0200

    up

--- c_scripts/1_es_riepilogo/8_funzioni.c
+++ c_scripts/1_es_riepilogo/8_funzioni.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // Dichiarazione della funzione per calcolare il fattoriale
-int fattoriale(int n);
+unsigned long long fattoriale(unsigned long long n);
 
 int main() {
-  int num;
+  unsigned long long num;
+  unsigned long long risultato;
 
   // Input dell'utente
   printf("Inserisci un numero per calcolare il fattoriale: ");
-  scanf("%d", &num);
+  scanf("%llu", &num);
+
+  // Avviso all'utente se il numero è negativo e conversione al valore assoluto
+  if ((long long)num < 0) {
+    printf("Hai inserito un numero negativo. Verrà considerato il valore assoluto.\n");
+    num = llabs((long long)num);
+  }
 
   // Calcolo e stampa del fattoriale
-  printf("Il fattoriale di %d è %d\n", num, fattoriale(num));
+  risultato = fattoriale(num);
+  printf("Il fattoriale di %llu è %llu\n", num, risultato);
 
   return 0;
 }
 
 // Definizione della funzione per calcolare il fattoriale
-int fattoriale(int n) {
+unsigned long long fattoriale(unsigned long long n) {
   if (n == 0) {
     return 1;
   } else {
