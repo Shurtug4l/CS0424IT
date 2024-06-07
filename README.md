0002df284a6125d26e1fc44095c5a7e386d454ef
Simone <92377343+simone0509@users.noreply.github.com>
2024-06-07 15:49:52 +0200

    up

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 Repository for my Cybersecurity Specialist course @ EPICODE -> CS0424IT.
 
-
 ![logo_ss](https://github.com/simone0509/CS0424IT/assets/92377343/1077c4c7-38a6-4be2-aee9-57ebde19fa4c)
--- UNIT_1/week2/S2L5/S2L5_bug_hunting/codice_corretto.c
+++ UNIT_1/week2/S2L5/S2L5_bug_hunting/codice_corretto.c
@@ -5,82 +5,77 @@ void moltiplica();
 void dividi();
 void ins_string();
 
-int main() 
-{
-    char scelta = '\0';
-    menu();
-    scanf(" %c", &scelta);
+int main() {
+  char scelta = '\0';
+  menu();
+  scanf(" %c", &scelta);
 
-    switch (scelta)
-    {
-        case 'A':
-        case 'a':
-            moltiplica();
-            break;
-        case 'B':
-        case 'b':
-            dividi();
-            break;
-        case 'C':
-        case 'c':
-            ins_string();
-            break;
-        default:
-            printf("Scelta non valida.\n");
-            break;
-    }
+  switch (scelta) {
+  case 'A':
+  case 'a':
+    moltiplica();
+    break;
+  case 'B':
+  case 'b':
+    dividi();
+    break;
+  case 'C':
+  case 'c':
+    ins_string();
+    break;
+  default:
+    printf("Scelta non valida.\n");
+    break;
+  }
 
-    return 0;
+  return 0;
 }
 
-void menu()
-{
-    printf("Benvenuto, sono un assistente digitale, posso aiutarti a sbrigare alcuni compiti\n");
-    printf("Come posso aiutarti?\n");
-    printf("A >> Moltiplicare due numeri\n");
-    printf("B >> Dividere due numeri\n");
-    printf("C >> Inserire una stringa\n");
+void menu() {
+  printf("Benvenuto, sono un assistente digitale, posso aiutarti a sbrigare "
+         "alcuni compiti\n");
+  printf("Come posso aiutarti?\n");
+  printf("A >> Moltiplicare due numeri\n");
+  printf("B >> Dividere due numeri\n");
+  printf("C >> Inserire una stringa\n");
 }
 
-void moltiplica()
-{
-    int a, b;
-    printf("Inserisci i due numeri da moltiplicare: ");
-    if (scanf("%d %d", &a, &b) != 2) {
-        printf("Errore: Input non valido.\n");
-        return;
-    }
+void moltiplica() {
+  int a, b;
+  printf("Inserisci i due numeri da moltiplicare: ");
+  if (scanf("%d %d", &a, &b) != 2) {
+    printf("Errore: Input non valido.\n");
+    return;
+  }
 
-    int prodotto = a * b;
-    printf("Il prodotto tra %d e %d è: %d\n", a, b, prodotto);
+  int prodotto = a * b;
+  printf("Il prodotto tra %d e %d è: %d\n", a, b, prodotto);
 }
 
-void dividi()
-{
-    int a, b;
-    printf("Inserisci il numeratore: ");
-    if (scanf("%d", &a) != 1) {
-        printf("Errore: Input non valido.\n");
-        return;
-    }
-    printf("Inserisci il denominatore: ");
-    if (scanf("%d", &b) != 1) {
-        printf("Errore: Input non valido.\n");
-        return;
-    }
-    if (b == 0) {
-        printf("Errore: Divisione per zero.\n");
-        return;
-    }
+void dividi() {
+  int a, b;
+  printf("Inserisci il numeratore: ");
+  if (scanf("%d", &a) != 1) {
+    printf("Errore: Input non valido.\n");
+    return;
+  }
+  printf("Inserisci il denominatore: ");
+  if (scanf("%d", &b) != 1) {
+    printf("Errore: Input non valido.\n");
+    return;
+  }
+  if (b == 0) {
+    printf("Errore: Divisione per zero.\n");
+    return;
+  }
 
-    float divisione = (float)a / b;
-    printf("La divisione tra %d e %d è: %.2f\n", a, b, divisione);
+  float divisione = (float)a / b;
+  printf("La divisione tra %d e %d è: %.2f\n", a, b, divisione);
 }
 
-void ins_string()
-{
-    char stringa[10];
-    printf("Inserisci la stringa (max 9 caratteri): ");
-    scanf("%9s", stringa);
-    printf("Hai inserito: %s\n", stringa);
+void ins_string() {
+  char stringa[10];
+  printf("Inserisci la stringa (max 9 caratteri): ");
+  scanf("%9s", stringa);
+  printf("Hai inserito: %s\n", stringa);
 }
