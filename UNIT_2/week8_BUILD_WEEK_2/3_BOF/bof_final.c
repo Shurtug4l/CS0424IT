f0f7e83585533c95ffe19cfdfeeac9e5174b6081
Simone <92377343+simone0509@users.noreply.github.com>
2024-07-15 16:08:34 +0200

    bof

--- UNIT_2/week8_BUILD_WEEK_2/3_BOF/bof_final.c
+++ UNIT_2/week8_BUILD_WEEK_2/3_BOF/bof_final.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
+#include <ctype.h>
+
+int check_if_integer(char *str)
+{
+  if (*str == '-' || *str == '+')
+    str++; // Salta il segno
+  if (!*str)
+    return 0; // Stringa vuota dopo il segno non valida
+  while (*str)
+  {
+    if (!isdigit(*str))
+      return 0; // Se non è una cifra, ritorna 0
+    str++;
+  }
+  return 1; // Tutti i caratteri sono cifre
+}
 
 // Funzione che provoca un segmentation fault accedendo a indici fuori dai
 // limiti
@@ -31,27 +48,33 @@ void normal_execution()
 {
   int vector[10], i, j, k;
   int swap_var;
+  char input[256];
 
-  printf("Inserire 10 interi:\n");
+  printf("\nInserire 10 interi:\n");
 
   for (i = 0; i < 10; i++)
   {
     int c = i + 1;
     printf("[%d]:", c);
 
-    // Controllo dell'input per assicurarsi che l'utente inserisca un numero
-    // intero
-    while (scanf("%d", &vector[i]) != 1)
+    while (1)
     {
-      printf("Input non valido! Inserisci un numero intero.\n");
-      while (getchar() != '\n')
-        ; // Svuota il buffer dell'input
-      printf("[%d]:", c);
+      scanf("%s", input);
+      if (check_if_integer(input))
+      {
+        vector[i] = atoi(input); // Conversione stringa -> intero
+        break;                   // Uscita dal ciclo se input valido
+      }
+      else
+      {
+        printf("Input non valido! Inserisci un numero intero.\n");
+        printf("[%d]:", c);
+      }
     }
   }
 
   // Stampa il vettore inserito
-  printf("Il vettore inserito è:\n");
+  printf("\nIl vettore inserito è:\n");
   for (i = 0; i < 10; i++)
   {
     int t = i + 1;
@@ -73,7 +96,7 @@ void normal_execution()
   }
 
   // Stampa il vettore ordinato
-  printf("Il vettore ordinato è:\n");
+  printf("\nIl vettore ordinato è:\n");
   for (j = 0; j < 10; j++)
   {
     int g = j + 1;
@@ -84,30 +107,37 @@ void normal_execution()
 int main()
 {
   int choice;
+  char input[256];
 
   // Menu di scelta per l'utente
-  printf("Menu:\n");
+  printf("\nMenu:\n");
   printf("1. Esegui normalmente\n");
   printf("2. Causa segmentation fault con accesso casuale\n");
   printf("3. Esci\n");
-  printf("Inserisci la tua scelta: ");
 
-  // Controllo dell'input dell'utente per una scelta valida
-  while (scanf("%d", &choice) != 1 || (choice < 1 || choice > 3))
+  while (1)
   {
+    printf("Inserisci la tua scelta: ");
+    scanf("%s", input);
+    if (check_if_integer(input))
+    {
+      choice = atoi(input);
+      if (choice >= 1 && choice <= 3)
+      {
+        break;
+      }
+    }
     printf("Scelta non valida! Inserisci 1, 2 o 3: ");
-    while (getchar() != '\n')
-      ; // Svuota il buffer dell'input
   }
 
   // Gestione delle diverse scelte dell'utente
-  if (choice == 2)
-  {
-    cause_segmentation_fault();
-  }
-  else if (choice == 1)
+  if (choice == 1)
   {
     normal_execution();
+  }
+  else if (choice == 2)
+  {
+    cause_segmentation_fault();
   }
   else if (choice == 3)
   {
